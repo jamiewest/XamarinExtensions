24b0bcad04eee3d01454cc31684d0ed1bb1eaeb0
Jamie West <jamiewst@gmail.com>
2019-03-12 19:49:58 -0700

    Updates

---
+++ src/Abstractions/INavigationService.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Threading.Tasks;
+
+namespace West.Extensions.Xamarin
+{
+    public  interface INavigationService
+    {
+        Task NavigateToAsync(Type destination);
+        Task NavigateToAsync(Type destination, object parameter);
+        Task NavigateToAsync<T>() where T : class;
+        Task NavigateToAsnc<T>(object parameter) where T : class;
+        Task NavigateBackAsync();
+    }
+}
